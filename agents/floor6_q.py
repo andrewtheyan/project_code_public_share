51695b88e2d5f6e273510724a324a5aee6d25fe9
Andrew Yan <srdrewyan@gmail.com>
2021-12-05 00:40:01 -0500

    added qmodel

--- agents/floor6_q.py
+++ agents/floor6_q.py
@@ -4,7 +4,6 @@ import os
 import numpy as np
 import pandas as pd
 import math
-import sys
 
 import torch
 import torch.nn as nn
@@ -20,11 +19,25 @@ class Agent(object):
         # Complications: pickle should work with any machine learning models
         # However, this does not work with custom defined classes, due to the way pickle operates
         # TODO you can replace this with your own model
-
-        self.ti = 0
-        self.last_block = []
-        self.curr_alpha0 = -0.2
-        self.curr_alpha1 = -0.5
+        self.exploration_proba = 0.01 # set to 1 for training
+        self.exploration_decreasing_decay = 0.001
+        self.min_exploration_proba = 0.01
+        self.gamma = 0.99
+        self.lr = 0.1
+        self.total_episode_reward = 0
+        self.rounds = 0
+        
+        self.last_state = None
+        self.last_action = None
+        self.curr_state = None
+        self.curr_action = None
+		
+        self.my_last_prices = None
+        self.opponent_last_prices = None
+        self.which_item_customer_bought= None
+        
+        self.filename = 'machine_learning_model/trained_model'
+        self.trained_model = pickle.load(open(self.filename, 'rb'))
 
         # self.nnfilename = 'machine_learning_model/nnpickle_model'
         # self.nn_model = pickle.load(open(self.nnfilename, 'rb'))
@@ -44,15 +57,176 @@ class Agent(object):
         with open(self.item1filename, 'rb') as f1:
             self.item1embedding = pickle.load(f1)
 
-        #TODO load pickled version of the table here
-        # self.tablepath = 'machine_learning_model/q_table'
-        # with open(self.tablepath, 'rb') as f2:
-        #     self.q_table = pickle.load(f2)
+        self.q_table_file = 'machine_learning_model/q_table_temp'
+        with open(self.q_table_file, 'rb') as f1:
+            self.q_table = pickle.load(f1)
 
 
+    def get_last_state_action(self):
+        """
+        if self.did_customer_buy_from_me:
+            if not math.isnan(self.which_item_customer_bought):
+                self.last_reward = self.my_last_prices[int(self.which_item_customer_bought)]
+            else:
+                self.last_reward = 1
+        else: 
+            if not math.isnan(self.which_item_customer_bought):
+                self.last_reward = -1 * self.opponent_last_prices[int(self.which_item_customer_bought)]
+            else:
+                self.last_reward = -1
+     	"""
+        if self.my_last_prices[0] == self.opponent_last_prices[0] and \
+            self.my_last_prices[1] == self.opponent_last_prices[1]:
+            #penalize same prices as opponent 
+            self.did_customer_buy_from_me = False
 
+         
+        if self.did_customer_buy_from_me:
+            self.last_reward = 5
+        else:
+            self.last_reward = -10
+            
+        state = [0,0]#np.subtract(self.my_last_prices, self.opponent_last_prices)
+     			
+        for i in range(len(state)):
+            if self.my_last_prices[i] - self.opponent_last_prices[i] > 0:
+                state[i] = 1
+            if self.my_last_prices[i] - self.opponent_last_prices[i] < 0:
+                state[i] = -1
+            else:
+                state[i] = 0
+
+        self.last_state = self.curr_state
+ 			
+        if state[0] == 1 and state[1] == 1:
+            self.curr_state = 0
+        if state[0] == 1 and state[1] == 0:
+            self.curr_state = 1
+        if state[0] == 0 and state[1] == 1:
+            self.curr_state = 2
+        if state[0] == 0 and state[1] == 0:
+            self.curr_state = 3
+        if state[0] == -1 and state[1] == -1:
+            self.curr_state = 4
+        if state[0] == -1 and state[1] == 0:
+            self.curr_state = 5
+        if state[0] == 0 and state[1] == -1:
+            self.curr_state = 6
+        if state[0] == -1 and state[1] == 1:
+            self.curr_state = 7
+        if state[0] == 1 and state[1] == -1:
+            self.curr_state = 8
+    			
+            
+    def update_table(self):
+        #print()
+        self.q_table[self.last_state, self.last_action] = (1-self.lr) * \
+            self.q_table[self.last_state, self.last_action] + \
+             self.lr*(self.last_reward + self.gamma*max(self.q_table[self.curr_state,:]))
+  
+        self.total_episode_reward = self.total_episode_reward + self.last_reward
+        
+        ###### Un comment the lines below for training
+        
+        #self.exploration_proba = max(self.min_exploration_proba, 
+        #                             np.exp(-self.exploration_decreasing_decay* self.rounds))#
+        #self.rounds += 1
+
+    		
+    def _process_last_sale(self, last_sale, profit_each_team):
+        # print("last_sale: ", last_sale)
+        # print("profit_each_team: ", profit_each_team)
+        my_current_profit = profit_each_team[self.this_agent_number]
+        opponent_current_profit = profit_each_team[self.opponent_number]
+
+        self.my_last_prices = last_sale[2][self.this_agent_number]
+        self.opponent_last_prices = last_sale[2][self.opponent_number]
+
+        self.did_customer_buy_from_me = last_sale[1] == self.this_agent_number
+        did_customer_buy_from_opponent = last_sale[1] == self.opponent_number
+
+        self.which_item_customer_bought = last_sale[0]
+        self.get_last_state_action()
+        self.update_table()
+		
+		
+        # print("My current profit: ", my_current_profit)
+        # print("Opponent current profit: ", opponent_current_profit)
+        # print("My last prices: ", my_last_prices)
+        # print("Opponent last prices: ", opponent_last_prices)
+        # print("Did customer buy from me: ", did_customer_buy_from_me)
+        # print("Did customer buy from opponent: ",
+        #       did_customer_buy_from_opponent)
+        # print("Which item customer bought: ", which_item_customer_bought)
+
+        # TODO - add your code here to potentially update your pricing strategy based on what happened in the last round
+
+        # price adjustment based on last 
+        #pass
+	
+    def choose_action(self):
+		
+        if np.random.uniform(0,1) < self.exploration_proba:
+            action = np.random.randint(9, size=1)[0]
+        else:
+            action = np.argmax(self.q_table[self.curr_state,:])
+            
+        self.last_action = self.curr_action 
+        self.curr_action = action
+        return action
+		    	
+		
+    def use_action(self, action):
+    	
+        up_p1 = 1.05
+        up_p2 = 1.05
+        down_p1 = 0.95
+        down_p2 = 0.95
+			
+        if self.opponent_last_prices[0] > self.my_last_prices[0]:
+            up_p1 = 1 + ((self.opponent_last_prices[0] / self.my_last_prices[0]) - 1) * 0.05
+        if self.opponent_last_prices[0] < self.my_last_prices[0]:
+            down_p1 = (self.opponent_last_prices[0] / self.my_last_prices[0])  - 0.15#* 0.5
+            if down_p1 <= 0:
+                down_p1 = self.opponent_last_prices[0] / self.my_last_prices[0]
+        if self.opponent_last_prices[1] > self.my_last_prices[1]:
+            up_p2 = 1 + ((self.opponent_last_prices[1] / self.my_last_prices[1]) - 1) * 0.05
+        if self.opponent_last_prices[1] < self.my_last_prices[1]:
+            down_p2 = (self.opponent_last_prices[1] / self.my_last_prices[1]) - 0.15#* 0.5
+            if down_p2 <= 0:
+                down_p2 = self.opponent_last_prices[1] / self.my_last_prices[1] 
+			
+        if (self.opponent_last_prices[0] <= 0 or self.opponent_last_prices[1] <= 0 or\
+            self.opponent_last_prices[0] >= 3 or self.opponent_last_prices[1] >= 4.5):
+				
+            up_p1 = 1.05
+            up_p2 = 1.05
+            down_p1 = 0.95
+            down_p2 = 0.95
+
+        if action == 0:
+            return 1, 1
+        if action == 1:
+            return 1, up_p2
+        if action == 2:
+            return up_p1, 1
+        if action == 3:
+            return up_p1, up_p2
+        if action == 4:
+            return 1, down_p2
+        if action == 5:
+            return down_p1, 1
+        if action == 6:
+            return down_p1, down_p2
+        if action == 7:
+            return down_p1, up_p2
+        if action == 8:
+            return up_p1, down_p2
+				
+    			
      # Search Models
-    def simple_gridsearch_rev(self,xmax, ymax,  user, ylow=0, xlow=0, ticks=10, silent=True, nn=False):
+    def simple_gridsearch_rev(self,xmax, ymax,  user, ylow=0, 
+    				xlow=0, ticks=10, silent=True, nn=False):
         model = self.nn_model
         x = []
         y = []
@@ -80,7 +254,9 @@ class Agent(object):
             
         return x, y, rev
 
-    def adv_gridsearch_rev(self, xmax, ymax, user, xlow=0, ylow=0, ticks=10, max_iter_=8, silent=True, nn=False):
+
+    def adv_gridsearch_rev(self, xmax, ymax, user, xlow=0, ylow=0,
+    				 ticks=10, max_iter_=8, silent=True, nn=False):
         # xmax=4.1, ymax=2.5, model=self.nn_model, user=X_train_tensor, nn=True, silent=True, max_iter_=15
        
         model = self.nn_model
@@ -117,7 +293,9 @@ class Agent(object):
             
         return best_rev, best_x, best_y
 
-    def rand_grad_search(self,xmax, ymax, user, x_start=0, y_start=0, max_iter_=100, silent=True, nn=False):
+
+    def rand_grad_search(self,xmax, ymax, user, x_start=0, y_start=0, 
+    				max_iter_=100, silent=True, nn=False):
         model = self.nn_model
         p_p_rev = 0
         prev_rev = -3
@@ -190,7 +368,9 @@ class Agent(object):
             #print(path[-1])
         return path, best_rev
 
-    def rand_init_adv_grid_search(self,xmax, ymax,  user, x_start=0, y_start=0, start_iter_=25, silent=True, nn=False):
+
+    def rand_init_adv_grid_search(self,xmax, ymax,  user, x_start=0, 
+    				y_start=0, start_iter_=25, silent=True, nn=False):
         model = self.nn_model
         init_xy, init_rev = self.rand_grad_search(ymax=ymax,xmax=xmax,
                                              user=user,
@@ -214,143 +394,15 @@ class Agent(object):
     
 
 
-    def _process_last_sale(self, last_sale, profit_each_team):
-        # if this is the beginning of the new game
-        self.ti +=1
-        if last_sale[2][self.this_agent_number][0] == np.nan:
-            return 1
-        # print("last_sale: ", last_sale)
-        # print("profit_each_team: ", profit_each_team)
-        my_current_profit = profit_each_team[self.this_agent_number]
-        opponent_current_profit = profit_each_team[self.opponent_number]
-
-        my_last_prices = last_sale[2][self.this_agent_number]
-        opponent_last_prices = last_sale[2][self.opponent_number]
-
-        did_customer_buy_from_me = last_sale[1] == self.this_agent_number
-        did_customer_buy_from_opponent = last_sale[1] == self.opponent_number
-
-        which_item_customer_bought = last_sale[0]
-
-        # print("My current profit: ", my_current_profit)
-        # print("Opponent current profit: ", opponent_current_profit)
-        # print("My last prices: ", my_last_prices)
-        # print("Opponent last prices: ", opponent_last_prices)
-        # print("Did customer buy from me: ", did_customer_buy_from_me)
-        # print("Did customer buy from opponent: ",
-        #       did_customer_buy_from_opponent)
-        # print("Which item customer bought: ", which_item_customer_bought)
-        x=0
-        if did_customer_buy_from_me:
-            x =1
-
-        y=0
-        if did_customer_buy_from_opponent:
-            y =1
-        #keep track of the last 100 sales
-        data = [which_item_customer_bought,x,y,my_last_prices,opponent_last_prices,my_current_profit, opponent_current_profit]
-        self.last_block.append(data)
 
 
-        n = 30
-        if len(self.last_block) == (2*n)+1:
-            self.last_block.pop(0)
-
- 
-        # only make price adjustment after every 50 T
-        if (self.ti > n) & (self.ti % n == 1 ):
-            # analyze last 50 T
-            self.analysis(None, self.last_block[-n:])
-            # if len(self.last_block) >50 :
-            #     self.analysis(self.last_block[-100:-50],self.last_block[-50:])
-            # else:
-            #     #TODO make policy decision
-            #     self.analysis(None, self.last_block[-50:])
-
-
-
-          
-
-    def analysis(self,old_list, new_list):
-        if old_list is None:
-            # change in profit differences thru out the 50 Ts
-            profit_diff = (new_list[-1][5] - new_list[-1][6]) - (new_list[0][5] - new_list[0][6])
-            n_me_0 = 0
-            n_me_1 = 0
-            # n_oppo_0 = 0
-            # n_oppo_1 = 0
-            item_0_loss = []
-            item_1_loss = []
-
-            #TODO if customer bought from neither team, consider lowering price
-
-            for result in new_list:
-                if result[0] ==0:
-                    n_me_0 += result[1]
-                    # n_oppo_0 += result[2]
-                elif result[0] ==1:
-                    n_me_1 += result[1]
-                    # n_oppo_1 += result[2]
-
-                # if customer bought from opponent, what is avg diferences in price
-                #item 0
-                if (result[2] == 1) & (result[0]== 0):
-                    my_p = result[3][0]
-                    oppo_p = result[4][0]
-                    item_0_loss.append(my_p - oppo_p)
-
-                #item 1
-                if (result[2] ==1) & (result[0]== 1):
-                    my_p = result[3][1]
-                    oppo_p = result[4][1]
-                    item_1_loss.append(my_p - oppo_p)
-
-
-            item_0_loss = self.remove_outlier(item_0_loss)
-            item_1_loss = self.remove_outlier(item_1_loss)
-            #remove outliers in item_0loss and item_1 loss
-            n_oppo_0 = len(item_0_loss)
-            n_oppo_1 = len(item_1_loss)
-            # self.curr_alpha0 = 1 * self.curr_alpha0
-            # self.curr_alpha1 = 1 * self.curr_alpha1
-
-            # if opponent outsell me 5 items for item0, I lower my price by that amount
-            if n_oppo_1 - n_me_1 >= 3:
-                if len(item_0_loss) != 0:
-                    avg0 = sum(item_0_loss)/len(item_0_loss)
-                    self.curr_alpha0 +=  -(avg0 + 0.02)
-            # if Im performing really raelly well, try incraesing my price
-            elif n_me_0 - n_oppo_0 >= 10:
-                # self.curr_alpha0 += abs(n_me_0 - n_oppo_0) * 0.005
-                if self.curr_alpha0 <0 :
-                    self.curr_alpha0 += min(0.07, abs(self.curr_alpha0))
-
-            # if opponent outsell me 5 items for item1, I lower my price by that amount
-            if n_oppo_1 - n_me_1 >= 3:
-                if len(item_1_loss) != 0:
-                    avg1 = sum(item_1_loss)/len(item_1_loss)
-                    self.curr_alpha1 +=  -(avg1 + 0.02)
-            elif n_me_1 - n_oppo_1 >= 10:
-                # self.curr_alpha0 += abs(n_me_0 - n_oppo_0) * 0.005
-                if self.curr_alpha1 <0 :
-                    self.curr_alpha1 += min(0.07, abs(self.curr_alpha1))
-                
-        #TODO run T/Z test to test out significant difference in last method tried.
-    def remove_outlier(self, l):
-        if len(l)<1:
-            return []
-        data = np.array(l)
-        return list(data[abs(data - np.mean(data)) < 2.1 * np.std(data)])
     # Given an observation which is #info for new buyer, information for last iteration, and current profit from each time
     # Covariates of the current buyer, and potentially embedding. Embedding may be None
     # Data from last iteration (which item customer purchased, who purchased from, prices for each agent for each item (2x2, where rows are agents and columns are items)))
     # Returns an action: a list of length n_items=2, indicating prices this agent is posting for each item.
     def action(self, obs):
         new_buyer_covariates, new_buyer_embedding, last_sale, profit_each_team = obs
-        # if this is the beginning of a new game
-        if last_sale[2][self.this_agent_number][0] == np.nan:
-            self.ti = 0
-            self.last_block = []
+
         #filling in missing user embedding
         if new_buyer_embedding is None :
             #output (1,13)
@@ -365,6 +417,7 @@ class Agent(object):
 
         i0_dot = np.dot(new_buyer_embedding, self.item0embedding)
         i1_dot = np.dot(new_buyer_embedding, self.item1embedding)
+
         #preparing Neural Network Input
         train_x = pd.DataFrame(new_buyer_covariates.reshape(1,-1), columns = ['Covariate 1', 'Covariate 2', 'Covariate 3'])
         train_x['price_item_0'] = 1
@@ -385,28 +438,24 @@ class Agent(object):
         # rev, x, y = self.adv_gridsearch_rev(xmax=4.1, ymax=2.5,user=X_train_tensor, nn=True, silent=True, max_iter_=8)
         
         self._process_last_sale(last_sale, profit_each_team)
+        
+        action = self.choose_action()
+        
+        discount0, discount1 = self.use_action(action)
+
+        price = [y*discount0,x*discount1]
+        # rev = rev
+
+
+
+        # print('last_sale()' + str(type(last_sale)) + 'contains: ')
+        # print(last_sale)
+
+        # print('profit_each_team()' + str(type(profit_each_team)) + 'contains: ')
+        # print(profit_each_team)
+
         # return self.trained_model.predict(np.array([1, 2, 3]).reshape(1, -1))[0] + random.random()
-
-        #if i got passed
-        # pre = self.last_block[-1]
-        # prepre = self.last_block[-2]
-        # if (self.ti >10) & (pre[-1] > pre[-2] ) & (prepre[-1] < prepre[-1] ):
-        #     self.curr_alpha0 = -0.4
-        #     self.curr_alpha0 = -1
-
-        self.check_alpha()
-
-        y = y + self.curr_alpha0
-        if y <= 0.000001:
-            y= 0.000001
-
-        x = x + self.curr_alpha1
-        if x <= 0.000001:
-            x= 0.000001
-        price = [y, x]
-
-        if self.ti <=7:
-            return[sys.float_info.min,sys.float_info.min]
+        print(price)
         return price
 
         # return [10,10]
@@ -416,11 +465,5 @@ class Agent(object):
 
 
         #TODO: implement grid search 
-    def check_alpha(self):
-        if self.curr_alpha0 > 0.4:
-            self.curr_alpha0 = 0.4
-
-        if self.curr_alpha1 > 0.8:
-            self.curr_alpha1 = 0.8
 
 
